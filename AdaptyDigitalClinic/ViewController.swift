4701f34d2dbc937975a73141a3f896c5bc92757f
Ravish Kumar <ravish.93srivastava@gmail.com>
2022-09-05 15:56:16 +0530

    initial commit

---
+++ AdaptyDigitalClinic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdaptyDigitalClinic
+//
+//  Created by Ravish Kumar on 05/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
